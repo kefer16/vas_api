21fe4b9d0be00ec78e4f72ea5fc449f7debabdea
kefer <morafi.1999.16@gmail.com>
2024-02-11 00:16:52 -0500

    se cambia url de documentacion

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { HttpExceptionFilter } from "./http-exception/http-exception.filter";
-
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
    app.useGlobalFilters(new HttpExceptionFilter());
@@ -11,10 +10,12 @@ async function bootstrap() {
       .setDescription("API Rest para uso exclusivo del aplicativo Vas")
       .setVersion("1.0")
       .addBearerAuth()
-      .setExternalDoc("JSON", "api-json")
+      .setExternalDoc("JSON", "swagger-json")
       .build();
+   app.setGlobalPrefix("v1");
    const document = SwaggerModule.createDocument(app, config);
-   SwaggerModule.setup("api", app, document);
+   SwaggerModule.setup("swagger", app, document);
+
    await app.listen(3000);
 }
 bootstrap();
