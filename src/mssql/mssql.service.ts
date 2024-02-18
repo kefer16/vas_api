f8650f9fea41fd615a144399af615252b2e37527
kefer16 <morafi.1999.16@gmail.com>
2024-02-18 18:22:06 -0500

    correccion de executeProcedure

--- src/mssql/mssql.service.ts
+++ src/mssql/mssql.service.ts
@@ -45,7 +45,7 @@ export class MSSQLService {
          });
          const result = await request.execute(nameProcedure);
 
-         return result.recordset[0].JSON;
+         return result.recordset[0];
       } catch (error) {
          throw new HttpException(
             `Error al ejecutar la consulta: ${error.message}`,
